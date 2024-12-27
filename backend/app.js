88fd063363ceb5f387b30299fc6f53f0c4bdbd61
zakaryadev03 <zakaryabelaid88@gmail.com>
2024-12-27 23:59:04 +0100

    finishing the base schemas

--- backend/app.js
+++ backend/app.js
@@ -4,5 +4,16 @@ const app = express();
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config({path:"backend/config/config.env" });
 }
+//using the middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//importing the routes
+const post = require("./routes/post");
+const user = require("./routes/user");
+
+//using the routes
+app.use("/api/v1", post);
+app.use("/api/v1", user);
 
 module.exports = app;
