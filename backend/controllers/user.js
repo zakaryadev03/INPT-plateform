66f3dc0329e90981a55c9c8ac5f20d3c23b6b950
zakaryadev03 <zakaryabelaid88@gmail.com>
2024-12-28 15:20:34 +0100

    login register models

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -26,3 +26,45 @@ exports.register = async (req, res) => {
         });
     }
 }
+
+exports.login = async (req, res) => {
+    try {
+      const { email, password} = req.body;
+
+      const user = await User.findOne({ email }).select("+password");
+
+      if(!user){
+        return res.status(400).json({
+          success: false,
+          message: "User doesnt exist"
+        });
+      }
+
+      const isMatch = await user.matchPassword(password);
+
+      if(!isMatch){
+        return res.status(400).json({
+          success: false,
+          message: "Password is incorrect"
+        });
+      }
+
+      const token = await user.generateToken();
+      const options = {
+        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+        httpOnly:true,
+      }
+      res.status(200)
+      .cookie("token", token, options)
+      .json({
+        success: true,
+        user,
+        token,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+}
