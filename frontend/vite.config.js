7dc1ed3c77d7aa03197441a78ba3c51b563b8fa5
zakaryadev03 <zakaryabelaid88@gmail.com>
2025-02-21 00:38:54 +0100

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+// https://vitejs.dev/config/
+export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
-		proxy: mode === "development" ? { 
-			"/api": { target: "http://localhost:5000", changeOrigin: true }
-		} : undefined, // Disable proxy in production
+		proxy: {
+			"/api": {
+				target: "http://backend:5000",
+				changeOrigin: true,
+			},
+		},
 	},
-}));
+});
